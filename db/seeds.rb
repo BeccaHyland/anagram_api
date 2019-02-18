372d2865d5a5590caf502e1ce48af48e4895620d
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-02-18 15:27:46 -0700

    transform dictionary.txt from product owner into Word model seeds with attributes for each word: spelling, length

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require './lib/dictionary.rb'
+
+Word.destroy_all
+
+if Rails.env == 'development' || Rails.env == 'production'
+  dictionary_array_of_hashes.each do |word_hash|
+    Word.create!(
+      spelling: word_hash.spelling,
+      length: word_hash.length
+    )
+  end
+  puts "Anagram API database successfully seeded"
+end
