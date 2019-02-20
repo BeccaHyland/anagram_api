a8ad1d0e37dc15cc690e05aaa2cca4afef70d2e8
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-02-20 08:39:13 -0700

    add guard to prevent POSTing words over 45 characters (the longest word in the dictionary)

--- spec/services/word_maker_spec.rb
+++ spec/services/word_maker_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 describe WordMaker do
   describe 'instance methods (unit testing)' do
+    describe '#new_words' do
+      it 'returns an array of Word objects' do
+        words_from_request = ["absotively", "posilutely"]
+        wm = WordMaker.new(words_from_request)
+
+        expect(wm.new_words.first).to be_a(Word)
+        expect(wm.new_words.last).to be_a(Word)
+      end
+    end
+
     describe '#parsed_words' do
       it 'parses array into word hashes formatted to db' do
         words_from_request = ["absotively", "posilutely"]
@@ -16,13 +26,15 @@ describe WordMaker do
       end
     end
 
-    describe '#new_words' do
-      it 'returns an array of Word objects' do
-        words_from_request = ["absotively", "posilutely"]
+    describe '#words_under_45' do
+      it 'removes any submitted words over 45 characters' do
+        words_from_request = ["absotively", "posilutely",
+                          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]
+
         wm = WordMaker.new(words_from_request)
 
-        expect(wm.new_words.first).to be_a(Word)
-        expect(wm.new_words.last).to be_a(Word)
+        expect(wm.words_under_45.length).to eq(2)
+        expect(wm.words_under_45).to_not include("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
       end
     end
   end
